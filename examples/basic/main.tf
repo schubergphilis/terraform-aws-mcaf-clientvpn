83a4ddb838880acd00efe5a2de94271d048eac64
Stephen Hoekstra <shoekstra@schubergphilis.com>
2024-08-08 11:54:26 +0200

    breaking: Rewrite for v1
    Rewritten with some improvements for v1:
    
    * Updated how certificate validation is done
    * Added option to bring your own certificate
    * Added option to bring your own security group(s) with it's own rules
    * Added Okta bookmark for users to access Self Service portal
    
    Also added a basic example.
    
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

---
+++ examples/basic/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 1.0"
+    }
+    okta = {
+      source  = "okta/okta"
+      version = ">= 1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 1.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+data "aws_route53_zone" "current" {
+  zone_id = "Z0123456789ABCDEFGHIJ"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = "clientvpn-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "kms" {
+  source  = "schubergphilis/mcaf-kms/aws"
+  version = "0.3.0"
+
+  name = "MyClientVPN"
+}
+
+module "security_group" {
+  source  = "schubergphilis/mcaf-security-group/aws"
+  version = "~> 2.0"
+
+  name_prefix = "MyClientVPN-sg-"
+  description = "Security group for the MyClientVPN Client VPN"
+  vpc_id      = module.vpc.vpc_id
+
+  egress_rules = {
+    allow_all = {
+      cidr_ipv4   = ["0.0.0.0/0"]
+      description = "Allow all outbound traffic"
+    }
+  }
+}
+
+module "clientvpn" {
+  source = "github.com/schubergphilis/terraform-aws-mcaf-clientvpn?ref=v1"
+
+  name               = "MyClientVPN"
+  client_cidr_block  = "172.16.0.0/16"
+  dns_servers        = ["8.8.8.8"]
+  kms_key_arn        = module.kms.arn
+  security_group_ids = [module.security_group.id]
+  subnet_ids         = module.vpc.private_subnets
+  zone_id            = data.aws_route53_zone.current.zone_id
+}
